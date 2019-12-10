e6d2b681cdf5cd40b6f533451488afb4862fc424
val <valfragier@gmail.com>
2019-12-10 12:14:03 -0500

    complete

--- src/main/java/io/zipcoder/persistenceapp/controllers/EmployeeController.java
+++ src/main/java/io/zipcoder/persistenceapp/controllers/EmployeeController.java
@@ -1,7 +1,39 @@
 package io.zipcoder.persistenceapp.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.zipcoder.persistenceapp.models.Employee;
+import io.zipcoder.persistenceapp.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
+    @Autowired
+    EmployeeService service;
+
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.service = employeeService;
+    }
+
+    @PostMapping("/employees/")
+    public ResponseEntity<Employee> create(@RequestBody Employee employee){
+        return new ResponseEntity<>(service.createEmployee(employee), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> read(@PathVariable Long id){
+        return new ResponseEntity<>(service.readEmployee(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> update(@PathVariable Long id, @RequestBody Employee employee){
+        return new ResponseEntity<>(service.updateEmployee(id, employee), HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> delete(@PathVariable Long id){
+        return new ResponseEntity<>(service.deleteEmployee(id), HttpStatus.OK);
+    }
 }
